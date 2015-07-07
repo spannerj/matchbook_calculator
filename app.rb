10415b94db4005ac72f547aa256547d2b35b589a
spannerj <spannerj@github.com>
2015-07-07 21:58:02 +0000

    under and overlay code sorted

--- app.rb
+++ app.rb
@@ -8,32 +8,51 @@ def calc_odds(back_odds, back_stake, lay_odds)
     #### work out standard lay
     
     # start with a penny lay stake
-    lay_stake = 0.01
-    cont = true
+    lay_stake = 9.70
+    lay_search = true
+    ul_search = true
+    ol_search = true
+    i = 0
     
     #loop until the back and lay results are equalised
-    while cont do
+    while lay_search || ul_search || ol_search do
         bw = back_wins(back_odds, back_stake, lay_stake, lay_odds)
         lw = lay_wins(back_stake, lay_stake)
         
+        puts 'ls - ' + lay_stake.to_s + ' bw - ' + bw.to_s + ' ---- lw ' + lw.to_s
+        
+        if (bw <= 0 || bw == 0) && ul_search
+            result_hash['underlaystake'] = lay_stake.round(2)
+            result_hash['underlaywin'] = bw * -1 #ensure no negative
+            result_hash['underlayloss'] = lw
+            ul_search = false
+        end
+            
         #increase the stake until the profit/loss equalises
         if bw > lw 
-            #increase the stake by a penny
-            lay_stake = lay_stake + 0.01
-        else
-            cont = false
+            result_hash['stake'] = lay_stake.round(2)
+            result_hash['cost'] = bw
+            result_hash['lay_cost'] = lw
+            lay_search = false
         end
+        
+        if (lw >= 0 || lw == 0) && ol_search
+            result_hash['overlaystake'] = lay_stake.round(2)
+            result_hash['overlaywin'] = bw
+            result_hash['overlayloss'] = lw #* -1 #ensure no negative
+            ol_search = false
+        end
+        
+        i += 1
+        if i == 100
+            ol_search = false
+            lay_search = false
+            ul_search = false
+        end    
+            
+        #increase the stake by a penny
+        lay_stake = lay_stake + 0.01
     end
-
-    result_hash['stake'] = lay_stake.round(2)
-    result_hash['cost'] = bw
-    result_hash['lay_cost'] = lw
-    
-    
-    #### work out underlay
-    
-    # result_hash['underlaystake'] = back_stake
-    # result_hash['underlosslose'] = 
     
     result_hash
 end
@@ -42,7 +61,6 @@ end
 def back_wins(back_odds, back_stake, lay_stake, lay_odds)
     
     back_return = back_stake * back_odds
-    
     back_profit = back_return - back_stake
     
     lay_liability = lay_stake * (lay_odds - 1)
@@ -64,6 +82,7 @@ end
 #format the currency
 def currency_format(number)
   if number < 0
+    puts number
     return "-£%.2f" % (number * -1).to_f 
   else
     return '£' + sprintf("%.02f", number)   
@@ -80,7 +99,13 @@ get '/' do
       @loss = currency_format(res_hash['cost'])
       @laystake = currency_format(res_hash['stake'])
       @lay_loss = currency_format(res_hash['lay_cost'])
-      #@underlaystake = currency_format(res_hash['underlaystake'])
+      @underlaystake = currency_format(res_hash['underlaystake'])
+#      puts 'ulw ' + res_hash['underlaywin'].to_s
+      @underlaywin = currency_format(res_hash['underlaywin'])
+      @underlayloss = currency_format(res_hash['underlayloss'])
+      @overlaystake = currency_format(res_hash['overlaystake']) 
+      @overlaywin = currency_format(res_hash['overlaywin']) 
+      @overlayloss = currency_format(res_hash['overlayloss'])
   end
 
   erb :calc
